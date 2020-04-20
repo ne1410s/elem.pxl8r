c36357bf7670b8e46074ac06fd0e17e68ee50ab9
Paul Jones <pjjones@siemens.com>
2020-04-20 16:17:11 +0100

    some img

--- README.md
+++ README.md
@@ -11,20 +11,23 @@
 ```
 - **src**: Sets a new image source
 - **contrast**: Sets the contrast 0 - 100
-- **pixels-x**: Sets the number of 
+- **x**: Sets the number of pixels in the *x* dimension
+- **y**: Sets the number of pixels in the *y* dimension
 
 ### Events
 ```javascript
 const tooltip = document.querySelector('ne14-pxl8r');
 
-tooltip.addEventListener('rendered', () => {
+tooltip.addEventListener('render', () => {
   console.log('Rendered');
 });
 ```
-- **rendered**: Fired on pixelation complete with the specified image and parameters
+- **change**: Fired when a parameter changes
+- **render**: Fired when pixelation occurs (e.g. after change in parameter(s))
 ### Methods
 - *There are no bespoke methods exposed by this element*
 ### Properties
 - `set` **src** (string): Sets the attribute with the corresponding value
 - **contrast** (number): Gets or sets the contrast 0 - 100
-- **pixels-x** (number): Gets or sets the number of pixels to use in the x-dimension. The aspect ratio is preserved in y.
+- **x** (number): Gets or sets the number of pixels in the *x* dimension. The aspect ratio is preserved for y, unless y is otherwise set
+- **y** (number): Gets or sets the number of pixels in the *y* dimension. The aspect ratio is preserved for x, unless x is otherwise set
--- src/pxl8r/pxl8r.ts
+++ src/pxl8r/pxl8r.ts
@@ -5,15 +5,51 @@ import stylesUrl from './pxl8r.css';
 
 export class Pxl8r extends CustomElementBase {
 
-  public static readonly observedAttributes = ['attrs!'];
+  public static readonly observedAttributes = ['src', 'contrast', 'x', 'y'];
+
+  private readonly _reader: FileReader = new FileReader();
+  private readonly _elemContrast: HTMLInputElement;
+  private readonly _elemPicker: HTMLInputElement;
+  private readonly _elemCanvas: HTMLCanvasElement;
+  private readonly _context: CanvasRenderingContext2D;
+
+  public set src(value: string) {
+    if (value) this.setAttribute('src', value);
+    else this.removeAttribute('src');
+  }
+
+  public set x(value: number) { this.setAttribute('x', `${value}`); }
+  public get x(): number { return parseInt(this.getAttribute('x')); }
+
+  public set y(value: number) { this.setAttribute('y', `${value}`); }
+  public get y(): number { return parseInt(this.getAttribute('y')); }
 
   constructor() {
     super(stylesUrl, markupUrl);
+
+    this._elemContrast = this.root.querySelector('#contrast');
+    this._elemPicker = this.root.querySelector('#filepicker');
+    this._elemCanvas = this.root.querySelector('#canvas');
+    this._context = this._elemCanvas.getContext('2d');
+    this._context.imageSmoothingEnabled = false;
+    
+    this._reader.addEventListener('load', e => this.setNewImageSource(e.target.result as string));
+    this._elemPicker.addEventListener('change', () => {
+      const file = this._elemPicker.files[0];
+      if (file) this._reader.readAsDataURL(file);
+      this.src = null;
+    });
   }
 
   attributeChangedCallback(name: string, oldValue: string, newValue: string) {
     switch (name) {
-      // case '': break;
+      case 'src':
+        this._elemPicker.value = null;
+        this.setNewImageSource(newValue);
+        break;
+      case 'contrast':
+        this._elemContrast.value = newValue;
+        break;
     }
   }
 
@@ -21,7 +57,39 @@ export class Pxl8r extends CustomElementBase {
     // on connected to dom
   }
 
-  disconnectedCallback() {
-    // on disconnected from dom
+  private setNewImageSource(src: string) {
+    if (!src) {
+      this._context.clearRect(0, 0, this._elemCanvas.width, this._elemCanvas.height);
+    } else {
+      const img = new Image();
+      img.addEventListener('load', e => this.onImageLoad(e));
+      img.src = src;
+    }
+  }
+
+  private onImageLoad(event: Event) {
+    const img = event.target as HTMLImageElement;
+    const aspect = img.width / img.height;
+
+    const targetW = this._elemCanvas.clientWidth;
+    const targetH = targetW / aspect;
+    
+    this._elemCanvas.width = targetW;
+    this._elemCanvas.height = targetH;
+    this._context.drawImage(img, 0, 0, targetW, targetH);
+  }
+
+  /** Emits a new event. */
+  private fire<T>(event: string, detail?: T) {
+    this.dispatchEvent(new CustomEvent(event, { detail }));
+  }
+
+  private debounce<T>(func: (arg: T) => void, delay = 200): (arg: T) => void {
+    let timeout: NodeJS.Timeout;
+    return function (arg) {
+      clearTimeout(timeout);
+      const that = this;
+      timeout = setTimeout(() => func.call(that, arg), delay);
+    };
   }
 }
